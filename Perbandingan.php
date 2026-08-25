cb8a841e76faafddecf074d03b6407b42ca435f4
FahrezaAja <fahrezaking@gmail.com>
2026-08-25 16:31:05 +0700

    perbandingan

---
+++ Perbandingan.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$a = 10;
+$b = 5;
+
+$a == $b; //sama dengan
+$a != $b; //tidak sama dengan
+$a > $b; //lebih besar dari
+$a < $b; //lebih kecil dari
+$a >= $b; //lebih besar sama dengan
+$a <= $b; //lebih kecil sama dengan
+$a === $b; //identik dengan
+$a !== $b; //tidak identik dengan
+$a <> $b; //tidak identik dengan
